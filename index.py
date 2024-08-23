5651effd29ce744de6821d1c47ac38ee3cc24ce6
Camilo <camilomunozb203@gmail.com>
2024-08-22 22:23:51 -0500

    Inicio de nuevo proyecto

---
+++ index.py
@@ -0,0 +1,22 @@
+while True:
+    try:
+        print(
+        """
+        Bienvenido al gestor de mensajes:
+        1. Generar un nuevo mensaje.
+        2. Ingresar un nuevo mensaje.
+        3. Ver todos los mensajes.
+        4. Salir.
+        """)
+        usuario = int(input("Ingrese una opcion: "))
+        if usuario == 1:
+            print("Proxima funcion")
+        elif usuario == 2:
+            print("Proxima funcion")
+        elif usuario == 3:
+            print("Proxima funcion")
+        elif usuario == 4: 
+            print("Muchas gracias por usar el gestor de mensajes, hasta pronto.")
+            break
+    except ValueError:
+        print("Error de digitacion, intenta nuevamente.")
